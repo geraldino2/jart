b84edc073bc0a9bbc289833e15dc97f25b7356bf
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-08-06 06:21:47 -0300

    add ns support

--- src/parse/massdns.py
+++ src/parse/massdns.py
@@ -24,14 +24,16 @@ def load(lines:list) -> (dict,set,set):
                     else:
                         valid[subdomain][1].add(terms[2][:-1])
             else:
+                code,subdomain = terms[2],terms[3][:-1]
                 if(lines[i+1] != ""):
-                    code,subdomain = terms[2],terms[3][:-1]
                     if(code != "NXDOMAIN"):
                         valid[subdomain] = (code,set())
                     else:
                         nxdomain_cname.add(subdomain)
-                elif(terms[2] in ["SERVFAIL","REFUSED"]):
-                    errors.add(terms[3][:-1])
+                elif(code == "NOERROR" and terms[5] == "NS"):
+                    valid[subdomain] = (code,set())
+                elif(code in ["SERVFAIL","REFUSED"]):
+                    errors.add(subdomain)
     return(valid,nxdomain_cname,errors)
 
 def load_from_file(filename:str) -> (dict,set,set):
