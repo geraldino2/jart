ba88f9ab62433bc34a844bbadd5e04b36479a1aa
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-08-10 04:26:17 -0300

    port scanning, db integration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 import mysql
+import re
+import sys
 from dns import rcode
 from modules.parse.collections import set_to_str
 from modules.parse import massdns
@@ -13,9 +15,37 @@ def run_command(cmd:str) -> (bytes,bytes,bytes):
                                 stderr=subprocess.PIPE)
     return(process.communicate()[0],process.communicate()[1])
 
+
+def get_dns_id(db_cursor,record:str) -> int:
+    db_cursor.execute(f"SELECT dns_id FROM ip_cnames WHERE record='{record}'")
+    result = db_cursor.fetchall()
+    if(len(result) > 0):
+        return(int(result[0][0]))
+    return(-1)
+
+def add_service(db_cursor,dns_id:int,port:int,transport_protocol:str,\
+                state:str,service:str,fingerprint:str):
+    if(dns_id == -1):
+        return(-1)
+    if(fingerprint == ""):
+        fingerprint = "NULL"
+    else:
+        fingerprint = f"'{fingerprint}'"
+    db_cursor.execute(f"INSERT INTO services (dns_id,port,service,state,\
+                        transport_protocol,fingerprint) SELECT '{dns_id}' AS \
+                        dns_id, {port} AS port, '{service}' AS service, \
+                        '{state}' AS state, '{transport_protocol}' AS \
+                        transport_protocol, {fingerprint} AS fingerprint\
+                        WHERE NOT EXISTS ( SELECT 1 FROM services WHERE \
+                        dns_id = {dns_id} AND port = {port} ) LIMIT 1;")
+
 def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
         db_credentials:tuple):
     os.system("cls||clear")
+
+    if(os.geteuid() != 0):
+        print("nmap/masscan requires sudo.")
+        sys.exit(1)
 
     print("#subfinder")
     _ = run_command(f"subfinder -d {domain} -all -o subfinder-out\
@@ -27,13 +57,13 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
 
     print("#massdns - resolve")
     _ = run_command(f"massdns -r {resolvers} -w massdns-resolve-out -o \
-        Srmldni amass-out -s 20000")
+                      Srmldni amass-out -s 20000")
 
     print("#create brute_wordlist")
     with open("tobrute","w") as saida:
-        for parametro in open(brute_wordlist,"r"):
-            _ = saida.write("{}.{}\n".format(parametro.replace("\n",""), \
-                domain))
+        for parameter in open(brute_wordlist,"r"):
+            _ = saida.write("{}.{}\n".format(parameter.replace("\n",""), \
+                              domain))
 
     print("#massdns - brute")
     _ = run_command(f"massdns -r {resolvers} -w massdns-brute-out -o \
@@ -45,8 +75,8 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
         open("t-subdomains","w") as valid_join, open("t-nxdomains","w") \
         as nxdomain_join, open("t-errors","w") as errors_join:
             valid,nxdomain,errors = massdns.load(\
-                                        resolve_out.read().split("\n") 
-                                        + brute_out.read().split("\n"))
+                                              resolve_out.read().split("\n") 
+                                              + brute_out.read().split("\n"))
             valid_join.write("\n".join(list(valid.keys())))
             nxdomain_join.write("\n".join(nxdomain))
             errors_join.write("\n".join(errors))
@@ -91,7 +121,6 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
             for domain in error:
                 if(domain not in valid):
                     errors_join.write(domain+"\n")
-            print(len(valid))
 
     print("#massdns - ns")
     _ = run_command(f"massdns -r /home/apolo2/.config/trusted-resolvers.txt \
@@ -114,6 +143,7 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
         ns_records = massdns.load(ns_lines)
         for subdomain in list(ns_records[0].keys()):
             if(subdomain not in subdomains_lines):
+
                 #1=dns.rdatatype.A 
                 query_result = dns_query.process_query("1.1.1.1",subdomain,1)
                 code = rcode.to_text(query_result[0])
@@ -146,10 +176,10 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
                         (subdomain_id), FOREIGN KEY (dns_id) \
                         REFERENCES ip_cnames(dns_id))")
     db_cursor.execute("CREATE TABLE services (dns_id INTEGER, port INTEGER, \
-                        subdomain_id INTEGER, fingerprint VARCHAR(1024), \
-                        PRIMARY KEY (dns_id, port), FOREIGN KEY (dns_id) \
-                        REFERENCES ip_cnames(dns_id), FOREIGN KEY \
-                        (subdomain_id) REFERENCES subdomains(subdomain_id))")
+                        state VARCHAR(14), service VARCHAR(32),\
+                        transport_protocol VARCHAR(3) NOT NULL, fingerprint\
+                        VARCHAR(307), PRIMARY KEY (dns_id, port), \
+                        FOREIGN KEY (dns_id) REFERENCES ip_cnames(dns_id))")
     db_cursor.execute("CREATE TABLE vulnerabilities (vulnerability_id INTEGER\
                         AUTO_INCREMENT, subdomain_id INTEGER NOT NULL, \
                         vulnerability VARCHAR(1024), PRIMARY KEY \
@@ -164,10 +194,9 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
 
     print("#table ip_cnames")
     ip_cnames = set()
-    for dns_type in [valid,nx,errors]:
-        for pair in ([ip_cname[1] for ip_cname in list(dns_type.values())]):
-            for value in pair:
-                ip_cnames.add(value)
+    for pair in ([ip_cname[1] for ip_cname in list(valid.keys())]):
+        for value in pair:
+            ip_cnames.add(value)
     ip_cnames_str = ""
     for value in ip_cnames:
         ip_cnames_str += f" ('{value}'),"
@@ -197,6 +226,65 @@ def run(domain:str,resolvers:str,brute_wordlist:str,alt_wordlist:str,\
                                         VALUES ('{subdomain}',{dns_id},\
                                         '{records}','{classification}')")
                     break
+    db.commit()
+
+    print("#ip-cnames/ips file")
+    ip_cname_link = dict()
+    with open("ips","w") as ips_file:
+        for ip_cname in ip_cnames:
+            ip = ip_cname
+            while(not re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ip)):
+                ip_query = dns_query.process_query("1.1.1.1",ip,1)
+                ip = ip_query[1].split("\n")[0]
+                if(ip == ""):
+                    break
+            if(ip != ""):
+                if(ip not in ip_cname_link.keys()):
+                    ips_file.write(ip + "\n")
+                ip_cname_link[ip] = ip_cname
+
+    print("#nmap")
+    _ = run_command("nmap -T4 --min-hostgroup 128 --max-hostgroup 2048 \
+                  --host-timeout 30m -max-retries 7 -sS -oG nmap-out -v \
+                  --open -iL ips --top-ports 2000 -n")
+
+    print("#masscan")split
+    _ = run_command("masscan -iL ips -p- --rate 20000 -oG masscan-out")
+
+    print("#add nmap to db")
+    with open("nmap-out","r") as nmap_file:
+        text = nmap_file.read()
+        for ip in ip_cname_link.keys():
+            text = text.replace(ip,ip_cname_link[ip])
+        for line in text.split("\n")[2:-2]:
+            if("Status: " not in line):
+                for terms in line.split("\t")[1].split(" ")[1:]:
+                    record = line.split("\t")[0].split(" ")[1]
+                    terms = terms.split("/")
+                    port = int(terms[0])
+                    transport_protocol = terms[2]
+                    state = terms[1]
+                    service = terms[4]
+                    fingerprint = terms[6]
+                    add_service(db_cursor,get_dns_id(db_cursor,record),port,\
+                                transport_protocol,state,service,fingerprint)
+    db.commit()
+
+    print("#add masscan to db")
+    with open("masscan-out","r") as masscan_file:
+        text = masscan_file.read()
+        for ip in ip_cname_link.keys():
+            text = text.replace(ip,ip_cname_link[ip])
+        for line in text.split("\n")[2:-2]:
+            line = line.split("\t")
+            record = line[1].split(" ")[1]
+            terms = line[2].split(" ")[1].split("/")
+            port = int(terms[0])
+            transport_protocol = terms[2]
+            state = terms[1]
+            service = terms[4]
+            add_service(db_cursor,get_dns_id(db_cursor,record),port,\
+                        transport_protocol,state,service,'')
     db.commit()
 
     print("#delete")
