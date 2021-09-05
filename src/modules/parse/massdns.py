723ae82dba3af267a3b3cd6d8eb53c46e712ed73
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-09-05 05:38:00 -0300

    refactoring

--- src/modules/parse/massdns.py
+++ src/modules/parse/massdns.py
@@ -1,4 +1,4 @@
-def load(lines:list) -> (dict,dict,set):
+def load(lines: list) -> (dict, dict, set):
     '''
     Input
         list
@@ -29,16 +29,16 @@ def load(lines:list) -> (dict,dict,set):
                 code,subdomain = terms[2],terms[3][:-1]
                 if(lines[i+1] != ""):
                     if(code != "NXDOMAIN"):
-                        valid[subdomain] = (code,set())
+                        valid[subdomain] = (code, set())
                     else:
-                        nxdomain_cname[subdomain] = (code,set())
+                        nxdomain_cname[subdomain] = (code, set())
                 elif(code == "NOERROR" and terms[5] == "NS"):
-                    valid[subdomain] = (code,set())
-                elif(code in ["SERVFAIL","REFUSED"]):
+                    valid[subdomain] = (code, set())
+                elif(code in ["SERVFAIL", "REFUSED"]):
                     errors.add(subdomain)
-    return(valid,nxdomain_cname,errors)
+    return(valid, nxdomain_cname, errors)
 
-def load_from_file(filename:str) -> (dict,dict,set):
+def load_from_file(filename: str) -> (dict, dict, set):
     '''
     Input
         str
